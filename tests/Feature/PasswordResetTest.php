924954ba5409ea2826662d48f1d9ca3324adb5d4
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2018-07-26 18:26:40 -0500

    Available password reset

--- tests/Feature/PasswordResetTest.php
+++ tests/Feature/PasswordResetTest.php
@@ -2,19 +2,37 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PasswordResetTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_visit_page_password_email()
     {
-        $this->assertTrue(true);
+        $this->get('/password/reset/efd886f6a2117dbaebd504e0d3ceac3f3c58a4458933cb88cb5ffe4e2217474e')
+            ->assertStatus(200)
+            ->assertSee("Reset Password");
+    }
+
+    /** @test */
+    public function a_user_can_reset_his_password()
+    {
+        $user = create('App\User', ["email" => "zaratedev@gmail.com"]);
+
+        $this->from('/password/reset')->post("/password/email", [
+            "email" => "zaratedev@gmail.com"
+        ]);
+
+
+
+        $response = $this->post('/password/reset', [
+            "token" => "",
+            "email" => "zaratedev@gmail.com",
+            "password" => "123456",
+            "password" => "123456"
+        ]);
     }
 }
